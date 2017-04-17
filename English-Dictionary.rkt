d8d340ee2758b150058304d124885ef52b74dc2e
Sokthai <Sokthai@users.noreply.github.com>
2017-04-16 22:14:56 -0400

    Update English-Dictionary.rkt
    play sound

--- English-Dictionary.rkt
+++ English-Dictionary.rkt
@@ -92,25 +92,51 @@
 ;------------
 
 
+(define word "")
+;(define app_id "app_id:da0194c3")
+;(define app_key "app_key:761ad80847bb97ee40842f7ecc43fade")
+;(define open_api "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/")
+
+
+
+;(define myrespond "")
+
+(define file-path "C:\\Users\\sokthai\\Downloads/")
+(define sound-url "")
+(define result "")
+
+
+(define macPath "/Users/sokthaitang/downloads/") ; path for mac
+(define winPath "C:\\Users\\thai\\Downloads\\") ; path for widnow
+(define ubuPath "/home/thai/Downloads/") ; pat for ubuntu
+
+
 (define (search w)
 
   
- 
+  ;(define result "")
+  (set! result "")
   (define con-url (string->url (string-append open_api w)))
   (define dict-port (get-pure-port con-url (list app_id app_key)))
   (define respond (port->string dict-port))
-
-  
+  ;(set! myrespond respond)
   (close-input-port dict-port)
   
-  (cond ((number? (string-contains respond "404 Not Found")) (printf "Not Found"))
+  (cond ((number? (string-contains respond "404 Not Found")) (set! result (list (list "Error:" "Not Found"))))
         (else
-         (searchDict (readjson-from-input respond) '|word| "word        : ")
-         (searchDict (readjson-from-input respond) '|definitions| "definitions : ")
-         (searchDict (readjson-from-input respond) '|examples| "examples    : ")
-         (searchDict (readjson-from-input respond) '|audioFile| "pronunciation : ")
+         (searchDict (readjson-from-input respond) '|word| "word:")
+         (searchDict (readjson-from-input respond) '|definitions| "definitions:")
+         (searchDict (readjson-from-input respond) '|examples| "examples:")
+         (searchDict (readjson-from-input respond) '|audioFile| "pronunciation:")
+         (let* ((audioURL (soundPath result)) 
+                (fileName (string-append macPath
+                                       (substring audioURL 43 (string-length audioURL)))))
+           (if (not (file-exists? fileName))
+               (send-url (soundPath result))
+               'ok)
+         )
   ))
- 
+  result
 )
 
 
@@ -133,34 +159,78 @@
                   (searchDict (hash-ref hash key) k des)))                  
                (else hash)))))
 
+
+
+
 (define (display hash k des)
-  (cond
-    
+  (cond  
     ((list? (hash-ref hash k))
      (cond
-       ((string? (car (hash-ref hash k))) (printf "~a~a\n" des (car (hash-ref hash k))))
+    
+       ((string? (car (hash-ref hash k))) (return  des (car (hash-ref hash k))))
        (else
         (show (hash-ref hash k) k des))))
-    (else (printf "~a~a\n" des (hash-ref hash k (lambda () ""))))
-  ))
+    
+    (else (return  des (hash-ref hash k (lambda () ""))))
+  )
+)
 
+
+
+
+(define (return des content )
+  
+  (if (list? result)
+      (set! result (append result (list (list des content))))
+      (set! result (list (list des content))))
+  
+  
+)
   
 
-
-
 (define (show lst k des)
+
   (cond ((null? lst) lst)
         (else
          (for (((key val) (in-hash (car lst )))) 
-           (printf "~a~a\n" des val ))
+          (return des val)
+           )
 
          (show (cdr lst) k des)
-         )))
+         ))
+  )
 
-(define (playSound path)
-  (play-sound path #t)
+
+
+
+(module file-player racket/base
+   (require racket/system)
+   (define player (find-executable-path "xmms"))
+   (define (play file)
+     (system* player (if (path? file) (path->string file) file)))
+   (provide play))
+
+
+
+
+;-----play mp3
+
+
+(define (soundPath lst)
+  
+  (if (and (list? lst) (null? (cdr lst))) 
+      (cadr (car lst))
+      (soundPath (cdr lst)))
 )
 
-(define (getSound url)
-  (send-url url)
-)
+
+(define (pronounce lst)
+  (let ((audioURL (soundPath lst)))
+    (cond  ((equal? lst "") '())
+           (else
+            (play-sound
+               (string-append macPath
+                                       (substring audioURL 43
+                                                  (string-length audioURL))) #t)))
+ )
+  )
